8cae8beac49abf4dea926594c6c55606c70595ef
WhyNotWhyYes <artemiy.zuzin@gmail.com>
2021-07-11 22:49:05 +0300

    imp

--- Navigation-2/Navigation/Profile/ProfileViewController.swift
+++ Navigation-2/Navigation/Profile/ProfileViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    @IBOutlet weak var HeaderView: ProfileHeaderView!    
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    @IBOutlet weak var HeaderView: ProfileHeaderView!
+    
+    let logInViewControler = LogInViewController()
   
-    var secondButton: UIButton = {
+    lazy var secondButton: UIButton = {
        let button = UIButton()
         
         button.setTitle("Button", for: .normal)
@@ -27,11 +30,21 @@ class ProfileViewController: UIViewController {
         self.view.backgroundColor = .lightGray
         
         view.addSubview(secondButton)
-        
+                
         secondButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([secondButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        
+        
+        NSLayoutConstraint.activate([secondButton.leadingAnchor.constraint(equalTo:
+                view.leadingAnchor),
             secondButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             secondButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             secondButton.widthAnchor.constraint(equalToConstant: 40)])
+                
+        logInViewControler.modalPresentationStyle = .fullScreen
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
+//        present(logInViewControler, animated: true, completion: nil)
+        navigationController?.pushViewController(LogInViewController(), animated: true)
     }
 }
